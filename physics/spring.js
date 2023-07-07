d78c7bba38366ab28e334f9577969b70cc1ef1b6
pfhaupt <pfhaupt@gmx.net>
2023-07-07 22:00:22 +0200

    Add Stress prototype
    Lots of hard coded values, but springs/connections can now break when too much pulls on them.

--- physics/spring.js
+++ physics/spring.js
@@ -1,25 +1,32 @@
 
 class Spring {
-    constructor(nodeA, nodeB, defaultRate = 5) {
+    constructor(nodeA, nodeB, defaultRate = 5, defaultRadius = 3) {
         this.start = nodeA;
         this.end = nodeB;
         this.rate = defaultRate;
+        this.radius = defaultRadius;
+        this.area = PI * this.radius * this.radius;
         this.restLength = dist(this.start.position.x, this.start.position.y, this.end.position.x, this.end.position.y);
+        this.stress = 0;
     }
 
     draw(canvas) {
         canvas.push();
-        canvas.stroke(255);
+        canvas.strokeWeight(this.radius);
+        canvas.stroke(lerpColor(color(0, 255, 0), color(255, 0, 0), this.stress));
         canvas.line(this.start.position.x, this.start.position.y, this.end.position.x, this.end.position.y);
         canvas.pop();
     }
 
     update() {
         let force = p5.Vector.sub(this.end.position, this.start.position);
-        let x = force.mag() - this.restLength;
+        let fMag = force.mag();
+        let x = fMag - this.restLength;
         x /= 10;
         force.normalize();
         force.mult(this.rate * x);
+        this.stress = 100 * force.mag() / this.area;
+        if (this.stress > 1.5) return false;
         this.start.applyForce(force);
         force.mult(-1);
         this.end.applyForce(force);
